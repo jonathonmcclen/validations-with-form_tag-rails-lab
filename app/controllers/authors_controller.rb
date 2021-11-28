4ab6548ecd41c0969198e9010714b136d98945ed
--replace-all <jonathonsmcclendon@gmail.com>
2021-11-28 16:10:06 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,39 @@
 class AuthorsController < ApplicationController
+
   def show
     @author = Author.find(params[:id])
+    render "/authors/show"
   end
 
   def new
     @author = Author.new
+    render "/authors/new"
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render new_author_path(@author)
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    @author = Author.find(params[:id])
+    render "/authors/edit"
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render "/authors/edit"
+    end
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,40 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    render "/posts/show"
+  end
+
+  def new
+    @post = Post.new
+    render "/posts/new"
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render new_post_path(@post)
+    end
   end
 
   def edit
     @post = Post.find(params[:id])
+    render "/posts/edit"
   end
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render "/posts/edit"
+    end 
 
-    redirect_to post_path(@post)
+    
   end
 
   private
